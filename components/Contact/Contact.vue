9c653167b7910d4165dd0356a301d5809cb68157
Luciano Kapluk <kaplukluciano@gmail.com>
2020-08-25 16:28:30 -0300

    deploy api

--- components/Contact/Contact.vue
+++ components/Contact/Contact.vue
@@ -15,6 +15,7 @@
                         <div class="clearfix"></div>
                         
                     </div>
+                  <no-ssr>
 
                     <form @submit.prevent="sendEmail">
                         <div class="row">
@@ -53,6 +54,7 @@
                     <div id="msgSubmit"  v-if="this.isEmpty" class="h3 text-center"><i class="fa fa-times"> </i> You must complete all the fields!</div>
                    <div id="msgSubmit"  v-if="this.msgError" class="h3 text-center"><i class="fa fa-times"> </i> Oops, something went wrong, try again later please!</div>
                     </form>
+                  </no-ssr>
 
                 </div>
             </div>
