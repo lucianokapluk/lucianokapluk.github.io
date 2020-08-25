1d08f0c8a1c53690f471b4e5506de42ef0c8a9d8
Luciano Kapluk <kaplukluciano@gmail.com>
2020-08-25 18:03:37 -0300

    fix node modules

--- components/Contact/Contact.vue
+++ components/Contact/Contact.vue
@@ -93,6 +93,8 @@ export default {
             headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin':'*',
+            'Access-Control-Allow-Headers': 'Content-Type'
   
           },
           body:JSON.stringify({
