972bdf4c47d46a204620df0d13ce75b2a728bfee
Luciano Kapluk <kaplukluciano@gmail.com>
2020-08-25 23:27:21 -0300

    fix and compressing css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,33 @@
 
 
-
+/* 'docs/css/bootstrap.css',
+'docs/css/animate.css',
+'docs/css/bootstrap.min.css',
+'docs/css/owl.carousel.css',
+'docs/css/owl.theme.css',
+'docs/css/responsive.css',
+'docs/css/style.css',
+'docs/css/swiper.min.css' */
+const files = ['docs/css/style.css'];
+    
+  files.forEach(element =>  {
   module.exports = function(grunt) {
 
     // Project configuration.
-    
-grunt.initConfig({
-    purifycss: {
-        options: {},
-        target: {
-            src: ['docs/index.html', 'docs/js/*.js'],
-            css:['docs/css/bootstrap.css',
-            'docs/css/animate.css',
-            'docs/css/bootstrap.min.css',
-            'docs/css/owl.carousel.css',
-            'docs/css/owl.theme.css',
-            'docs/css/responsive.css',
-            'docs/css/style.css',
-            'docs/css/swiper.min.css'],
-            dest: 'css/sape.css',
-        }
-    }
-  });
+
+              grunt.initConfig({
+                  purifycss: {
+                      options: {},
+                      target: {
+                          src: ['docs/index.html', 'docs/js/*.js'],
+                          css:[element],
+                          dest:'css/' + element,
+                      }
+                  }
+                });
+              
+         
+
   
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -30,3 +36,4 @@ grunt.initConfig({
     grunt.registerTask('default', ['uglify']);
   
   };
+});
