ae8273447593543a9d66060a75c21b6b7f0cac26
Luciano Kapluk <kaplukluciano@gmail.com>
2020-08-26 18:47:19 -0300

    add webp image format

--- components/Courses/Courses.vue
+++ components/Courses/Courses.vue
@@ -14,7 +14,7 @@
                     <div class="col-lg-3 col-md-6 col-sm-12" >
                     <div class="item">
                         <div class="thumbnail">
-                            <img src="img/courses/flutter.png" alt="..." class="img-circle team-img">
+                            <img src="img/courses/flutter.webp" alt="..." class="img-circle team-img">
                             <div class="caption">
                                 <h3>Flutter</h3>
                                 <p>Udemy / Fernadno Herrera</p>
@@ -28,7 +28,7 @@
                     <div class="col-lg-3 col-md-6 col-sm-12" >
                     <div class="item ">
                         <div class="thumbnail">
-                            <img src="img/courses/reac.png" alt="..." class="img-circle team-img">
+                            <img src="img/courses/reac.webp" alt="..." class="img-circle team-img">
                             <div class="caption">
                                 <h3>React Js</h3>
                                 <p>Platzi / Richard Kaufman </p>
@@ -53,7 +53,7 @@
                     <div class="col-lg-3 col-md-6 col-sm-12" >
                     <div class="item ">
                         <div class="thumbnail">
-                            <img src="img/courses/js.png" alt="..." class="img-circle team-img">
+                            <img src="img/courses/js.webp" alt="..." class="img-circle team-img">
                             <div class="caption">
                                 <h3>Javascript Basics</h3>
                                 <p>Platzi / Sacha Lifszyc</p>
