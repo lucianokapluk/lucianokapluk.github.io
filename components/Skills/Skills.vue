4ee2abb2cbd9113c4b922aacd58693ea59aa3bdc
Luciano Kapluk <kaplukluciano@gmail.com>
2020-08-27 20:24:14 -0300

    add courses and favicon

--- components/Skills/Skills.vue
+++ components/Skills/Skills.vue
@@ -55,7 +55,63 @@
             </a>
             <div class="collapse" id="collapseExample">
                 
-                <Courses />
+                       <div id="tf-courses" class="text-center">
+                <div id="team" class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-12" >
+                    <div class="item">
+                        <div class="thumbnail">
+                            <img src="img/courses/flutter.webp" alt="..." class="img-circle team-img">
+                            <div class="caption">
+                                <h3>Flutter</h3>
+                                <p>Udemy / Fernadno Herrera</p>
+                                
+                            </div>
+                        </div>
+                    </div>
+
+
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12" >
+                    <div class="item ">
+                        <div class="thumbnail">
+                            <img src="img/courses/reac.webp" alt="..." class="img-circle team-img">
+                            <div class="caption">
+                                <h3>React Js</h3>
+                                <p>Platzi / Richard Kaufman </p>
+                                
+                            </div>
+                        </div>
+                    </div>
+
+                    </div>
+                    
+                    <div class="col-lg-3 col-md-6 col-sm-12" >
+                    <div class="item ">
+                        <div class="thumbnail">
+                            <img src="img/courses/rn.png" alt="..." class="img-circle team-img">
+                            <div class="caption">
+                                <h3>React Native</h3>
+                                <p>Platzi / Leonidas Esteban</p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-12" >
+                    <div class="item ">
+                        <div class="thumbnail">
+                            <img src="img/courses/js.webp" alt="..." class="img-circle team-img">
+                            <div class="caption">
+                                <h3>Javascript Basics</h3>
+                                <p>Platzi / Sacha Lifszyc</p>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+         
+                </div>
+        </div>
                 
             </div>
             </div>
@@ -67,13 +123,10 @@
 </template>
 
 <script>
-import Courses from '../Courses/Courses';
+
 export default {
     name: 'Skills',
-    components:{
-        Courses,
-       
-    }
+   
     
 }
 </script>
