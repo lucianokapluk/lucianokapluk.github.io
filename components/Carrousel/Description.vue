e64898da26e217df3006ef2084bde90e8fcf804d
Luciano Kapluk <kaplukluciano@gmail.com>
2020-09-03 18:49:09 -0300

    deploy

--- components/Carrousel/Description.vue
+++ components/Carrousel/Description.vue
@@ -4,7 +4,7 @@
            <h5>{{this.proyectdata.name}}</h5> 
              <a :href="this.proyectdata.url" target="_blank" rel="noopener noreferrer">{{this.proyectdata.url}}</a> 
             <h4>{{this.proyectdata.how}}</h4>
-            <a :href="this.proyectdata.github" target="_blank" rel="noopener noreferrer"><h4>take a look at the code in github</h4></a>
+            <a :href="this.proyectdata.github " target="_blank" rel="noopener noreferrer"><h4>take a look at the code in github</h4></a>
             <h4>  <span class="date">  <i class="fa fa-calendar"> </i> {{this.proyectdata.date}}</span></h4>
 
         </div>
