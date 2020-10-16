75bdaf6b961e9f84cf847c790daba0fd477b1112
Luciano Kapluk <kaplukluciano@gmail.com>
2020-10-16 18:23:48 -0300

    add download link app

--- components/Carrousel/Description.vue
+++ components/Carrousel/Description.vue
@@ -5,6 +5,8 @@
              <a :href="this.proyectdata.url" target="_blank" rel="noopener noreferrer">{{this.proyectdata.url}}</a> 
             <h4>{{this.proyectdata.how}}</h4>
             <a :href="this.proyectdata.github " target="_blank" rel="noopener noreferrer"><h4>take a look at the code in github</h4></a>
+            
+            <a v-if="this.proyectdata.urldownload != ''" :href="this.proyectdata.urldownload" target="_blank" rel="noopener noreferrer"><h5>Download the app</h5></a>
             <h4>  <span class="date">  <i class="fa fa-calendar"> </i> {{this.proyectdata.date}}</span></h4>
 
         </div>
@@ -17,6 +19,7 @@ export default {
     props: ['proyectdata'] ,
     
 }
+
 </script>
 <style  scoped>
 
